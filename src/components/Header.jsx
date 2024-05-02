289d8d91435567cbe56594018178e25b390694f5
SugalAden <112473563+SugalAden@users.noreply.github.com>
2024-05-02 03:13:43 +0200

    Fully set up responsive nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,19 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 const Header = () => {
   return (
     <Navbar expand="lg" style={{ backgroundColor: "#FFA100" }}>
-      <Container>
+      <Container fluid>
         <Navbar.Brand href="#home">
           <img
-            src="your-logo.png"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
+            src="/HOLIDAZE Logo - BigCommerce Store Logo with Transparent Background.png"
+            className="d-inline-block align-top img-fluid "
             alt="Your logo"
+            style={{ maxWidth: "160px" }}
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse
-          id="basic-navbar-nav"
-          className="justify-content-end primary"
+          id="responsive-navbar-nav"
+          className="justify-content-end"
         >
           <Nav>
             <Nav.Link href="#home" className="text-white">
