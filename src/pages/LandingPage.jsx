dd52f081f1fc295bf229298bb2b4f784201802c1
SugalAden <112473563+SugalAden@users.noreply.github.com>
2024-05-09 01:16:08 +0200

    Few changes made for the mobile and desktop view

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,59 +3,66 @@ import { Button, Col, Container, Image, Row } from "react-bootstrap";
 
 const LandingPage = () => {
   return (
-    <Container className="py-5">
+    <>
       {/* Mobile layout */}
-      <Row className="justify-content-center align-items-center d-md-none">
-        <Col xs={11} sm={8}>
-          <Image src="/Group2.png" alt="Holiday Venue" fluid rounded />
-        </Col>
-        <Col xs={11} sm={8} className="text-center">
-          <h2 className="font-weight-bold text-wrap fs-4">
-            Find Your Perfect Holiday <br /> Venue with Holidaze!
-          </h2>
-          <p className="text-muted text-wrap fs-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed
-            massa blandit, faucibus quam sed, feugiat massa.
-          </p>
-          <Button
-            className="rounded-pill"
-            style={{ backgroundColor: "#FFA100" }}
-          >
-            View Venues
-          </Button>
-        </Col>
-      </Row>
+      <Container className="d-md-none">
+        <Row className="justify-content-center ">
+          <Col xs={11} sm={8}>
+            <Image src="/Group2.png" alt="Holiday Venue" fluid rounded />
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs={11} sm={8} className="text-center">
+            <h2 className="font-weight-bold text-wrap fs-4">
+              Find Your Perfect Holiday <br /> Venue with Holidaze!
+            </h2>
+            <p className="text-muted text-wrap fs-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              sed massa blandit, faucibus quam sed, feugiat massa.
+            </p>
+            <Button
+              className="rounded-pill"
+              style={{ backgroundColor: "#FFA100" }}
+            >
+              View Venues
+            </Button>
+          </Col>
+        </Row>
+      </Container>
 
       {/* Desktop layout */}
-      <Row className="justify-content-center align-items-center d-none d-md-flex">
-        <Col md={6} lg={6} className="text-md-right">
-          <h2 className="font-weight-bold text-wrap fs-1">
-            Find Your
-            <br /> Perfect Holiday <br /> Venue with Holidaze!
-          </h2>
-          <p className="text-muted text-wrap fs-6">
-            Lorem ipsum dolor sit amet, consectetur <br />
-            adipiscing elit. Integer sed massa blandit,
-            <br /> faucibus quam sed, feugiat massa.
-          </p>
-          <Button
-            className="rounded-pill"
-            style={{ backgroundColor: "#FFA100" }}
-          >
-            View Venues
-          </Button>
-        </Col>
-        <Col md={6} className="text-md-left ml-lg-5">
-          <Image
-            src="/Group2.png"
-            alt="Holiday Venue"
-            fluid
-            rounded
-            style={{ maxWidth: "90%" }}
-          />
-        </Col>
-      </Row>
-    </Container>
+      <Container className="py-5 d-none d-md-block">
+        <Row className="justify-content-center align-items-center">
+          <Col md={6} lg={6} className="text-md-right">
+            <h2 className="font-weight-bold text-wrap fs-1">
+              Find Your
+              <br /> Perfect Holiday <br /> Venue with Holidaze!
+            </h2>
+            <p className="text-muted text-wrap fs-6">
+              Lorem ipsum dolor sit amet, consectetur <br />
+              adipiscing elit. Integer sed massa blandit,
+              <br /> faucibus quam sed, feugiat massa.
+            </p>
+            <Button
+              className="rounded-pill"
+              style={{ backgroundColor: "#FFA100" }}
+            >
+              View Venues
+            </Button>
+          </Col>
+          <Col md={6} className="text-md-left">
+            <Image
+              src="/Group2.png"
+              alt="Holiday Venue"
+              fluid
+              rounded
+              style={{ maxWidth: "90%" }}
+              className="ml-md-5"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
