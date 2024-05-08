9d21dc41098fe58560abb7abf5954bcdde2ec8ff
SugalAden <112473563+SugalAden@users.noreply.github.com>
2024-05-08 04:49:02 +0200

    Desktop view finished

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,10 +4,13 @@ import { Button, Col, Container, Image, Row } from "react-bootstrap";
 const LandingPage = () => {
   return (
     <Container className="py-5">
-      <Row className="justify-content-center">
-        <Col xs={11} sm={8} md={6} className="text-center">
+      {/* Mobile layout */}
+      <Row className="justify-content-center align-items-center d-md-none">
+        <Col xs={11} sm={8}>
           <Image src="/Group2.png" alt="Holiday Venue" fluid rounded />
-          <h2 className="mt-3 font-weight-bold text-wrap fs-4">
+        </Col>
+        <Col xs={11} sm={8} className="text-center">
+          <h2 className="font-weight-bold text-wrap fs-4">
             Find Your Perfect Holiday <br /> Venue with Holidaze!
           </h2>
           <p className="text-muted text-wrap fs-6">
@@ -20,6 +23,36 @@ const LandingPage = () => {
           >
             View Venues
           </Button>
+        </Col>
+      </Row>
+
+      {/* Desktop layout */}
+      <Row className="justify-content-center align-items-center d-none d-md-flex">
+        <Col md={6} lg={6} className="text-md-right">
+          <h2 className="font-weight-bold text-wrap fs-1">
+            Find Your
+            <br /> Perfect Holiday <br /> Venue with Holidaze!
+          </h2>
+          <p className="text-muted text-wrap fs-6">
+            Lorem ipsum dolor sit amet, consectetur <br />
+            adipiscing elit. Integer sed massa blandit,
+            <br /> faucibus quam sed, feugiat massa.
+          </p>
+          <Button
+            className="rounded-pill"
+            style={{ backgroundColor: "#FFA100" }}
+          >
+            View Venues
+          </Button>
+        </Col>
+        <Col md={6} className="text-md-left ml-lg-5">
+          <Image
+            src="/Group2.png"
+            alt="Holiday Venue"
+            fluid
+            rounded
+            style={{ maxWidth: "90%" }}
+          />
         </Col>
       </Row>
     </Container>
