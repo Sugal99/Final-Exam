ee6973caf8af205cdff0d5840040db680f39ab5b
SugalAden <112473563+SugalAden@users.noreply.github.com>
2024-05-14 01:47:19 +0200

    Rendered api data to cards, and added text utils

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,22 +1,53 @@
-import React from "react";
-import { Card, Button, ListGroup, Row, Col } from "react-bootstrap";
+// ProductCard.jsx
+import React, { useEffect, useState } from "react";
+import { Card, Button, Row, Col } from "react-bootstrap";
+import { getAllVenues } from "./services.jsx/api/VenuesApi";
+import { truncateText } from "./utils/textUtils";
 
 const ProductCard = () => {
+  const [venues, setVenues] = useState([]);
+
+  useEffect(() => {
+    const fetchVenues = async () => {
+      try {
+        const { data } = await getAllVenues();
+        console.log("Venues data:", data); // Log data to console
+        setVenues(data); // Ensure data is an array
+      } catch (error) {
+        console.error("Error fetching venues:", error);
+      }
+    };
+
+    fetchVenues();
+  }, []);
+
   return (
     <Row xs={1} md={2} lg={3} className="g-4 mt-5">
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card>
-            <Card.Img variant="top" src="/AdobeStock_325677709.jpeg" />
-            <Card.Body>
-              <Card.Title>Product Title</Card.Title>
-              <Card.Text>This is a short description of the product.</Card.Text>
-              <Card.Text style={{ color: "green" }}>100$</Card.Text>
-              <Button variant="primary">View Venues</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
+      {Array.isArray(venues) &&
+        venues.map((venue) => (
+          <Col key={venue.id}>
+            <Card>
+              <Card.Img
+                variant="top"
+                src={venue.media && venue.media[0] ? venue.media[0].url : ""}
+              />
+              <Card.Body style={{ position: "relative" }}>
+                <Card.Text className="text-muted mb-0 position-absolute top-0 end-0 ml-3 p-1">
+                  {venue.name}
+                </Card.Text>
+                <Card.Title>{venue.name}</Card.Title>
+                <Card.Text>{truncateText(venue.description, 100)}</Card.Text>
+                <Card.Text style={{ color: "green" }}>{venue.price}$</Card.Text>
+                <Button
+                  variant="primary"
+                  style={{ backgroundColor: "#FFA100" }}
+                >
+                  View Venues
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
     </Row>
   );
 };
---
+++ src/components/utils/textUtils.jsx
@@ -0,0 +1,8 @@
+// textUtils.js
+export const truncateText = (text, maxLength) => {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + "...";
+  } else {
+    return text;
+  }
+};
