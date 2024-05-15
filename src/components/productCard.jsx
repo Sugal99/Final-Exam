817df8af222a46670e4fe1bc480bf3ef8bdbe933
SugalAden <112473563+SugalAden@users.noreply.github.com>
2024-05-15 02:21:27 +0200

    fixing rendering and height of cards

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,4 +1,3 @@
-// ProductCard.jsx
 import React, { useEffect, useState } from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
 import { getAllVenues } from "./services.jsx/api/VenuesApi";
@@ -23,31 +22,42 @@ const ProductCard = () => {
 
   return (
     <Row xs={1} md={2} lg={3} className="g-4 mt-5">
-      {Array.isArray(venues) &&
-        venues.map((venue) => (
-          <Col key={venue.id}>
-            <Card>
+      {venues.map((venue) => (
+        <Col key={venue.id}>
+          <Card className="h-100">
+            <div>
               <Card.Img
                 variant="top"
                 src={venue.media && venue.media[0] ? venue.media[0].url : ""}
+                className="card-img-top"
+                style={{ height: "200px", objectFit: "cover" }}
               />
-              <Card.Body style={{ position: "relative" }}>
-                <Card.Text className="text-muted mb-0 position-absolute top-0 end-0 ml-3 p-1">
-                  {venue.name}
-                </Card.Text>
-                <Card.Title>{venue.name}</Card.Title>
-                <Card.Text>{truncateText(venue.description, 100)}</Card.Text>
-                <Card.Text style={{ color: "green" }}>{venue.price}$</Card.Text>
-                <Button
-                  variant="primary"
-                  style={{ backgroundColor: "#FFA100" }}
-                >
-                  View Venues
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+            </div>
+            <Card.Body className="d-flex flex-column position-relative">
+              <Card.Text className="text-muted position-absolute top-0 end-0">
+                yoyoyoy
+              </Card.Text>
+              <Card.Title>{truncateText(venue.name, 22)}</Card.Title>
+              <Card.Text className="description">
+                {venue.description
+                  ? truncateText(venue.description, 40)
+                  : "No description available yet!"}
+              </Card.Text>
+              <Card.Text style={{ color: "green" }}>${venue.price}</Card.Text>
+              <Button
+                variant="primary"
+                style={{
+                  backgroundColor: "#FFA100",
+                  marginTop: "auto",
+                  maxWidth: "135px",
+                }}
+              >
+                View Venues
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
     </Row>
   );
 };
