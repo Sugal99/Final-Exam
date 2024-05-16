1c17b826c7bed7e0aff7b7815bdeca18bf694ffa
SugalAden <112473563+SugalAden@users.noreply.github.com>
2024-05-16 03:41:28 +0200

    Added venue rating for the cards

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
 import { getAllVenues } from "./services.jsx/api/VenuesApi";
 import { truncateText } from "./utils/textUtils";
+import { StarFill } from "react-bootstrap-icons";
 
 const ProductCard = () => {
   const [venues, setVenues] = useState([]);
@@ -25,7 +26,7 @@ const ProductCard = () => {
       {Array.isArray(venues) &&
         venues.map((venue) => (
           <Col key={venue.id}>
-            <Card className="h-100">
+            <Card className="h-100 position-relative">
               <div>
                 <Card.Img
                   variant="top"
@@ -34,7 +35,7 @@ const ProductCard = () => {
                   style={{ height: "200px", objectFit: "cover" }}
                 />
               </div>
-              <Card.Body className="position-relative">
+              <Card.Body>
                 <div className="d-flex align-items-center mb-3">
                   {venue.owner.avatar && (
                     <img
@@ -51,15 +52,28 @@ const ProductCard = () => {
                 <Card.Title>{truncateText(venue.name, 22)}</Card.Title>
                 <Card.Text>
                   {venue.description
-                    ? truncateText(venue.description, 40)
+                    ? truncateText(venue.description, 30)
                     : "No description available yet!"}
                 </Card.Text>
+                <div className="position-absolute bottom-0 end-0 m-3 py-1">
+                  {Array(5)
+                    .fill()
+                    .map((_, index) => (
+                      <StarFill
+                        key={index}
+                        className={
+                          index < venue.rating
+                            ? "text-warning me-1"
+                            : "text-muted me-1"
+                        }
+                      />
+                    ))}
+                </div>
                 <Card.Text style={{ color: "green" }}>${venue.price}</Card.Text>
                 <Button
                   variant="primary"
                   style={{
                     backgroundColor: "#FFA100",
-                    marginTop: "auto",
                     maxWidth: "135px",
                   }}
                 >
